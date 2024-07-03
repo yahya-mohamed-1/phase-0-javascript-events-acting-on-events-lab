04ed1dc2786f7c135555b43411d70fd3969bb307
Yahya Mohamed <171671702+yahya-mohamed-1@users.noreply.github.com>
2024-07-03 15:42:53 +0300

    Complete assignment submission

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
